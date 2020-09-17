e8bc773824ae8e4707543e4697ffe8f1ce311437
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-17 07:29:51 +0700

    good morning

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/good')
+  getGoodMorning() : Record<string, unknown> {
+    return this.appService.getGoodMorning()
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getGoodMorning(): { message: string } {
+    return { message: 'Good Morning' };
+  }
 }
